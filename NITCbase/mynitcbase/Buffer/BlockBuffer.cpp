cd11c1a6bf5714796c59096415ee811cad71d9df
ShifanaSShfk <shifanasshafeek01@gmail.com>
2024-10-28 16:20:16 +0530

    s3 e1 - BB SB Main RCT ACT Main ORT

--- NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
+++ NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
@@ -3,46 +3,139 @@
 #include <cstdlib>
 #include <cstring>
 
+//  Stage 2     :   BB(blockNum)
+                //  RB(int blockNum), 
+                //  BB::getHeader(struct HeadInfo *head), 
+                //  RB::getRecord(union Attribute *rec, int slotNum)
+
+//  Stage 3     :
+        //  || read from the BUFFER instead of the disk directly ||
+        //  || uses loadBlockAndGetBufferPtr() method instead of Disk::readBlock() ||
+        
+                //  BB::getHeader       Modified
+                //  RB::getRecord       Modified
+
+                //  RB::setRecord(union Attribute *rec, int slotNum), *************DK!!!*************FROM WHERE THIS CAME
+                //  BB::loadBlockAndGetBufferPtr(unsigned char **buffPtr)
+
 BlockBuffer::BlockBuffer(int blockNum) {
     this->blockNum = blockNum;
 }
 
 RecBuffer::RecBuffer(int blockNum) : BlockBuffer::BlockBuffer(blockNum) {}
 
+
+/*
+Used to get the header of the block into the location pointed to by `head`
+NOTE: this function expects the caller to allocate memory for `head`
+*/
 int BlockBuffer::getHeader(struct HeadInfo* head) {
-    unsigned char buffer[BLOCK_SIZE];
-    Disk::readBlock(buffer, this->blockNum);
+    
+  unsigned char buffer[BLOCK_SIZE];
+  unsigned char *bufferPtr;
+  int ret = loadBlockAndGetBufferPtr(&bufferPtr);
 
-    memcpy(&head->numSlots, buffer + 24, 4);
-    memcpy(&head->numEntries, buffer + 16, 4);
-    memcpy(&head->numAttrs, buffer + 20, 4);
-    memcpy(&head->lblock, buffer + 8, 4);
-    memcpy(&head->rblock, buffer + 12, 4);
+  if (ret != SUCCESS) {
+    return ret;   
+    // return any errors that might have occured in the process
+  }
 
-    return SUCCESS;
+
+  // read the block at this.blockNum into the buffer
+  Disk::readBlock(buffer, this->blockNum);
+
+  // populate the numEntries, numAttrs and numSlots fields in *head
+  memcpy(&head->numSlots, buffer + 24, 4);
+  memcpy(&head->numEntries, buffer + 16, 4);
+  memcpy(&head->numAttrs, buffer + 20, 4);
+  memcpy(&head->rblock, buffer + 12, 4);
+  memcpy(&head->lblock, buffer + 8, 4);
+
+  return SUCCESS;
 }
 
 
 int RecBuffer::getRecord(union Attribute* rec, int slotNum) {
-    struct HeadInfo head;
+  
+  struct HeadInfo head;
+  unsigned char *bufferPtr;
+  int ret = loadBlockAndGetBufferPtr(&bufferPtr);
 
-    this->getHeader(&head);
+  if (ret != SUCCESS) {
+    return ret;
+  }
 
-    int attrCount = head.numAttrs;
-    int slotCount = head.numSlots;
+  // get the header using this.getHeader() function
+  this->getHeader(&head);
 
-    unsigned char buffer[BLOCK_SIZE];
-    Disk::readBlock(buffer, this->blockNum);
+  int attrCount = head.numAttrs;
+  int slotCount = head.numSlots;
 
-    // header size -> 32
-    // slotMapSize -> numSlots
-    // index -> recordSize*slotNum
+  unsigned char buffer[BLOCK_SIZE];
 
-    int recordSize = attrCount * ATTR_SIZE;
-    unsigned char* slotPointer = buffer + 32 + slotCount + recordSize*slotNum;
+  // read the block at this.blockNum into a buffer
+  Disk::readBlock(buffer, this->blockNum);
 
-    memcpy(rec, slotPointer, recordSize);
+  /* record at slotNum will be at offset HEADER_SIZE + slotMapSize + (recordSize * slotNum)
+     - each record will have size attrCount * ATTR_SIZE
+     - slotMap will be of size slotCount
+  */
+  int recordSize = attrCount * ATTR_SIZE;
+  unsigned char *slotPointer = buffer + HEADER_SIZE + slotCount + (recordSize * slotNum);
 
+  // load the record into the rec data structure
+  memcpy(rec, slotPointer, recordSize);
+  
+  return SUCCESS;
+}
 
-    return SUCCESS;
+int RecBuffer::setRecord(union Attribute *rec, int slotNum) {
+  struct HeadInfo head;
+
+  // get the header using this.getHeader() function
+  this->getHeader(&head);
+
+  int attrCount = head.numAttrs;
+  int slotCount = head.numSlots;
+
+  // read the block at this.blockNum into a buffer
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer, this->blockNum);
+
+  /* record at slotNum will be at offset HEADER_SIZE + slotMapSize + (recordSize * slotNum)
+     - each record will have size attrCount * ATTR_SIZE
+     - slotMap will be of size slotCount
+  */
+  int recordSize = attrCount * ATTR_SIZE;
+  unsigned char *slotPointer = buffer + 32 + slotCount + recordSize * slotNum;
+
+  // load the record into the rec data structure
+  memcpy(slotPointer, rec, recordSize);
+  Disk::writeBlock(buffer,this->blockNum);
+
+  return SUCCESS;
+}
+
+/*
+Used to load a block to the buffer and get a pointer to it.
+NOTE: this function expects the caller to allocate memory for the argument
+*/
+int BlockBuffer::loadBlockAndGetBufferPtr(unsigned char **buffPtr) {
+  // check whether the block is already present in the buffer using StaticBuffer.getBufferNum()
+  int bufferNum = StaticBuffer::getBufferNum(this->blockNum);
+
+  if (bufferNum == E_BLOCKNOTINBUFFER) {
+    bufferNum = StaticBuffer::getFreeBuffer(this->blockNum);
+
+    if (bufferNum == E_OUTOFBOUND) {
+      return E_OUTOFBOUND;
+    }
+
+    Disk::readBlock(StaticBuffer::blocks[bufferNum], this->blockNum);
+  }
+
+  // store the pointer to this buffer (blocks[bufferNum]) in *buffPtr
+  *buffPtr = StaticBuffer::blocks[bufferNum];
+
+  return SUCCESS;
 }
--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -4,125 +4,24 @@
 #include "FrontendInterface/FrontendInterface.h"
 
 
-/*
-//  --- DISPLAYING (not using rblock)
+void printRelAttr() {
+  for (int i = 0; i < 2; i++) {
+    RelCatEntry relCatBuf;
+    RelCacheTable::getRelCatEntry(i, &relCatBuf);
 
-  // create objects for the relation catalog and attribute catalog
-  RecBuffer relCatBuffer(RELCAT_BLOCK);
-  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+    printf("Relation: %s\n", relCatBuf.relName);
 
-  HeadInfo relCatHeader;
-  HeadInfo attrCatHeader;
-
-  // load the headers of both the blocks into relCatHeader and attrCatHeader.
-  // (we will implement these functions later)
-  relCatBuffer.getHeader(&relCatHeader);
-  attrCatBuffer.getHeader(&attrCatHeader);
-
-  // i = 0 to total relation count 
-  for (int i = 0; i < relCatHeader.numEntries; i++) {
-
-    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
-
-    relCatBuffer.getRecord(relCatRecord, i);
-
-    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
-
-    // j = 0 to number of entries in the attribute catalog 
-    for (int j = 0; j < attrCatHeader.numEntries; j++) {
-
-      // declare attrCatRecord and load the attribute catalog entry into it
-      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
-      attrCatBuffer.getRecord(attrCatRecord, j);
-      
-      // attribute catalog entry corresponds to the current relation 
-      if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0) {
-        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
-        // get the attribute name 
-        printf("  %s: %s\n",attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
-      }
+    for (int j = 0; j < relCatBuf.numAttrs; j++) {
+      AttrCatEntry attrCatBuf;
+      AttrCacheTable::getAttrCatEntry(i, j, &attrCatBuf);
+      const char* attrType = (attrCatBuf.attrType == NUMBER)
+        ? "NUM"
+        : "STR";
+      printf("\t%s : %s\n", attrCatBuf.attrName, attrType);
     }
-    printf("\n");
   }
-
-  return 0;
 }
-*/
 
-
-
-/*
-//  --- DISPLAYING (using rblock) --- EXERCISE 1
-
-int main(int argc, char *argv[]) {
-  Disk disk_run;
-  // Buffer Initialization -- RecBuffer objects
-  RecBuffer relCatBuffer(RELCAT_BLOCK);
-  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
-
-
-  // Header Info
-  HeadInfo relCatHeader;
-  HeadInfo attrCatHeader;
-  
-  // Original attribute catalog header
-  HeadInfo attrCatHeader_store;
-
-  relCatBuffer.getHeader(&relCatHeader);
-  attrCatBuffer.getHeader(&attrCatHeader);
-  
-  attrCatHeader_store = attrCatHeader;
-
-
-  int total_no_of_rel = relCatHeader.numEntries;
-  int no_of_attr;
-  int count;
-  
-  
-  // Loop through Relations
-  // Total number of relations 
-  for(int i = 0; i < total_no_of_rel; i++) {
-  
-    Attribute relCatRecord[RELCAT_NO_ATTRS];
-    relCatBuffer.getRecord(relCatRecord, i);
-    
-    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
-    no_of_attr = relCatRecord[RELCAT_NO_ATTRIBUTES_INDEX].nVal;
-    count = 0;
-    
-    // Loop through Attributes
-    while(count < no_of_attr){
-    
-    	//std::cout<<"entry = "<<attrCatHeader.numEntries<<" slots="<<attrCatHeader.numSlots;
-    	    		
-    	for(int j = 0; j < attrCatHeader.numEntries; j++) {
-      		Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
-      		attrCatBuffer.getRecord(attrCatRecord, j);
-
-      if(!strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal)) {
-        count++;
-        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM":"STR";
-        printf(" %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
-      }
-    }
-    if(count < no_of_attr){
-    	attrCatBuffer = RecBuffer(attrCatHeader.rblock);
-    	attrCatBuffer.getHeader(&attrCatHeader);
-    	}
-    }
-    attrCatHeader = attrCatHeader_store;
-    printf("\n");
-  }
-
-  // return FrontendInterface::handleFrontend(argc, argv);
-  return 0;
-}
-*/
-
-
-/*
-//  --- DISPLAY ATTRIBUTE NAME CHANGE --- EXERCISE 2
-*/
 void printAllRelationsAndAttributes() {
   RecBuffer relCatBuffer(RELCAT_BLOCK);
   HeadInfo relCatHeader;
@@ -149,7 +48,7 @@ void printAllRelationsAndAttributes() {
         attrCatBuffer.getRecord(attrCatRecord, j);
         if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal))
           continue;
-          
+
 
         const char* attrType = (attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER)
           ? "NUM"
@@ -181,7 +80,6 @@ void exercise2() {
           Disk::readBlock(buffer, ATTRCAT_BLOCK);
           memcpy(buffer + 52 + 96*j + 16, "Batch", ATTR_SIZE);
           Disk::writeBlock(buffer, ATTRCAT_BLOCK);
-
           printAllRelationsAndAttributes();
         }
       }
@@ -189,10 +87,20 @@ void exercise2() {
   }
 }
 
+
 int main(int argc, char *argv[]) {
-  // Initialize the Run Copy of Disk 
+  /* Initialize the Run Copy of Disk */
   Disk disk_run;
+  StaticBuffer buffer;
+  OpenRelTable cache;
+
+  //  DISPLAY CATALOG ATTRIBUTES (using buffer ans=d cache)
+  printRelAttr();
+
+  // Class to Batch change
   exercise2();
+
+  //  Print all RELATIONS & ATTRIBUTES
   printAllRelationsAndAttributes();
 
   return FrontendInterface::handleFrontend(argc, argv);
